17333fe014a69cb0ee543c7c7b54d065c08954c3
Jaime A. Esquivel <jaimeesquivelgomez@gmail.com>
2016-08-08 21:23:02 -0600

    first api project

---
+++ MyQuiz/MyQuiz/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MyQuiz.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ MyQuiz/MyQuiz/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MyQuiz.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
